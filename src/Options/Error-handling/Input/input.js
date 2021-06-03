60dad6943e82f5405ca37f07d1e0962c159d57d2
Curstantine <resistlyfe@gmail.com>
2021-06-03 23:24:38 +0530

    v3 initial

--- src/Options/Error-handling/Input/input.js
+++
@@ -1,6 +0,0 @@
-const idCheck = require("./id.js");
-const keyCheck = require("./keywords.js");
-const pageCheck = require("./page.js");
-const sortCheck = require("./sort.js");
-
-module.exports = { idCheck, keyCheck, pageCheck, sortCheck };
