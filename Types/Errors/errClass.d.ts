b54a94a29eb728211f08c7c4e1738c296361b36b
Rachala Ovin <resistlyfe@gmail.com>
2021-07-26 15:53:34 +0530

    Cleaning Commit

--- Types/Errors/errClass.d.ts
+++
@@ -1,7 +0,0 @@
-export class KongouError extends Error {
-    constructor(code: any, message: any);
-}
-export class KongouClientError extends KongouError {
-}
-export class KongouServerError extends KongouError {
-}
